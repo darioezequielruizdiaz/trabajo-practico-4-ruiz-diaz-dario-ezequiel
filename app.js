9ec7263737331b3abc34d58eedfe19b0531df6dd
Dario Ezequiel Ruiz Diaz <117091511+darioezequielruizdiaz@users.noreply.github.com>
2025-06-26 14:08:22 -0300

    Configuramos la conexion a la base de datos y configuramos las variables de entornos

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 import express from "express"
 import chalk from "chalk";
+import dotenv from "dotenv";
+import { startDB } from "./src/config/database.js";
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Iniciamos la base de datos
+startDB();
+
+// Hacemos que la app escuche el puerto y deje un mensaje en caso de que 
 app.listen(PORT, () => {
     console.log(`EL servidor se está ejecutando en ${chalk.bgMagenta(`http://localhost:${PORT}/`)}`);
 })
---
+++ src/config/database.js
@@ -0,0 +1,34 @@
+import { Sequelize } from "sequelize";
+import chalk from "chalk";
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+// Intanciamos la clase de Sequelize
+export const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT
+    }
+)
+
+// Funcion para iniciar la conexion a la base de datos
+export const startDB = async () => {
+    try {
+        await sequelize.authenticate()
+        console.log(chalk.bgGreen.black("Conexión exitosa a la base de datos "))
+        await sequelize.sync()
+    } catch (error) {
+        console.log("\n" + chalk.red("Error al conectar con la base de datos:"))
+        console.log(chalk.redBright("────────────────────────────────────────────"));
+        console.log(`${chalk.yellow("Nombre:")} ${error.name}`);
+        console.log(`${chalk.yellow("Mensaje:")} ${error.message || "No hay mensaje"}`);
+        if (error.original?.code) {
+            console.log(`${chalk.yellow("Código:")} ${error.original.code}`)
+        }
+    console.log(chalk.redBright("────────────────────────────────────────────\n"))
+    }
+}
